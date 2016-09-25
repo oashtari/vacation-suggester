e798cc03120f309bf533892cb51da680119e27f1
Omid Ashtari <oashtari@gmail.com>
2016-09-25 09:52:17 -0700

    add bootstrap file, jquery file, jquery code in script file

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// back end
+
+
+// front end
+
+$(document).ready(function(){
+  $("submit#push").click(function(event) {
+
+    var name = $("#name").val();
+    var continent = $("#continent").val();
+    var landscape = $("#landscape").val();
+    var social = $("#social").val();
+    var age = parseInt($("#age").val());
+  })
+
+})
